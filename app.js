d30a8eb18d1dacf7d428cf8e023a38c6dec29c6c
Tawhid <gem@tawhid.fios-router.home>
2020-03-06 12:35:16 -0500

    initial commit for timer

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const getBody = document.querySelector('.recievedBox');
 const getLike = document.querySelector('.like');
 const getLikeNum = document.querySelector('.likeNum');
+const getTimerButton = document.querySelector('.timerButton');
+const getTime = document.querySelector('.time');
 
+
+// Like Button 
 let like = 0;
 
 increaseLike = () => {
@@ -15,3 +19,11 @@ likeClick = () => {
 }
 
 getLike.addEventListener(('click'), likeClick)
+
+// //////////////////////////////////////////////////////////////////
+
+// Timer
+let time = 0;
+
+
+
